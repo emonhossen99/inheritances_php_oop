231dd713a19b084814aa75696145dda32cd6d655
ahamedemon99 <mdxhamedemon@gmail.com>
2023-01-07 19:07:22 +0600

    frist time added

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include('main.php');
+
+$admiation = new Main();
+
+echo "Your Name is : ".$admiation->studentName = "Md Emon Hossen </br>";
+echo "Your Roll is : ".$admiation->studentRoll = 164513 ."</br>";
+echo "Your Department Name is : ".$admiation->studentDepartment = "Computer science And Engrneering</br>";
+
+
+?>
---
+++ main.php
@@ -0,0 +1,8 @@
+<?php
+include('submain.php');
+
+class Main extends subMain{
+  public $studentName;
+  public $studentRoll;
+}
+?>
